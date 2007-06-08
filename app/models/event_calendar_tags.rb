a4bae2db1b2da3a1491d40f95b9e1a6f2698efc6
deploy <deploy@126ae6b6-1cb0-44cf-83d6-85411bde8332>
2007-06-08 01:30:29 +0000

    Initial documentation of tags.
    git-svn-id: svn+ssh://svn.hellovenado.com/var/svn/tribe_cms_extensions/event_calendar@38 126ae6b6-1cb0-44cf-83d6-85411bde8332

--- app/models/event_calendar_tags.rb
+++ app/models/event_calendar_tags.rb
@@ -43,10 +43,14 @@ module EventCalendarTags
     tag.expand
   end
   
+  desc %{  <r:calendar:category />
+          The category which the current event calendar is in. }    
   tag "calendar:category" do |tag|
     tag.locals.event_search.category
   end
 
+  desc %{  <r:calendar:slugs />
+          The comma separated list of "slugs" included in the current set of events.  }    
   tag "calendar:slugs" do |tag|
     # unless tag.locals.event_search.slugs.downcase == "all" || tag.locals.event_search.slugs.blank? || tag.locals.event_search.slugs.
       tag.locals.calendars.collect { |c| c.slug }.join(", ")
@@ -55,6 +59,8 @@ module EventCalendarTags
     # end 
   end
    
+  desc %{  <r:calendar:names />
+          The comma separated list of calendar names included in the current set of events ("all" if all).  }    
   tag "calendar:names" do |tag|
     unless tag.locals.event_search.slugs.downcase == "all"
       tag.locals.calendars.collect { |c| c.name }.join(", ") unless tag.locals.calendars.nil?
@@ -63,28 +69,36 @@ module EventCalendarTags
     end
   end
    
+  desc %{  <r:calendar:period />
+          The name of the period for the current set of events.  }    
   tag "calendar:period" do |tag|
     tag.locals.event_search.period
   end
 
+  desc %{  <r:calendar:periods />
+          The number of periods the set of events spans.  }    
   tag "calendar:periods" do |tag|
     tag.locals.event_search.period.amount
   end
   
-  tag "calendar:description" do |tag|
-    tag.locals.event.description
-  end
-    
+  desc %{  <r:calendar:begin_date format="" />
+          The beginning date of the period of the currently selected events.  }    
   tag "calendar:begin_date" do |tag|
     format = tag.attr['format'].nil? ? "%d %b %y" : tag.attr['format']
     tag.locals.event_search.period.begin_date.strftime(format)
   end
    
+  desc %{  <r:calendar:end_date format="" />
+          The end date of the period of the currently selected events.  }    
   tag "calendar:end_date" do |tag|
     format = tag.attr['format'].nil? ? "%d %b %y" : tag.attr['format']
     tag.locals.event_search.period.end_date.strftime(format)    
   end
-  
+
+  desc %{  <r:calendar:nav format="" class="" />
+          Container for calendar navigation links. Append :week, :month or :year to the end of 
+          this tag to generate a link for each respective period which will always be relative to the 
+          currently selected begin date. }      
   tag "calendar:nav" do |tag|
     tag.expand
   end
@@ -98,11 +112,13 @@ module EventCalendarTags
       }
     end
   end
-  
+
   tag "calendar:event" do |tag|      
     tag.expand
   end
   
+  desc %{  <r:calendar:event:each />
+          The main container tag / iterator for all events in the currently selected period and calendar set. }        
   tag "calendar:event:each" do |tag|
     events = tag.locals.events
     content = ''
@@ -119,6 +135,8 @@ module EventCalendarTags
     content
   end
   
+  desc %{  <r:calendar:event:each:header />
+          Repeat the content between these tags once everytime it changes. }        
   tag 'calendar:event:each:header' do |tag|
     previous_headers = tag.locals.previous_headers
     name = tag.attr['name'] || :unnamed
@@ -135,53 +153,78 @@ module EventCalendarTags
     end
   end
    
+  desc %{  <r:calendar:event:each:calendar_name />
+          The name of the calendar which this event is associated with. }        
   tag "calendar:event:each:calendar_name" do |tag|
     tag.locals.event.calendar.name
   end
    
-  tag "calendar:event:each:id" do |tag|
-    tag.locals.event.id
-  end
-   
+  desc %{  <r:calendar:event:each:calendar_description />
+          The description of the calendar assocociated with this event. }
   tag "calendar:event:each:calendar_description" do |tag|
     tag.locals.event.calendar.description
   end
    
+  desc %{  <r:calendar:event:each:id />
+          The id of the current event. }
+  tag "calendar:event:each:id" do |tag|
+    tag.locals.event.id
+  end
+   
+  desc %{  <r:calendar:event:each:title />
+          The title of the event. }
   tag "calendar:event:each:title" do |tag|
     tag.locals.event.title
   end
    
+  desc %{  <r:calendar:event:each:description />
+          The full description of the event. }
   tag "calendar:event:each:description" do |tag|
     tag.locals.event.description
   end
    
+  desc %{  <r:calendar:event:each:location />
+          The location of the event. }
   tag "calendar:event:each:location" do |tag|
     tag.locals.event.location
   end
-   
-  tag "calendar:event:each:timeperiod" do |tag|
-    if tag.locals.event.allday then
-      timeperiod = "All Day"
-    else
-      timeperiod = tag.locals.event.start_date.strftime(tag.attr['format']) << " - " << tag.locals.event.end_date.strftime(tag.attr['format'])
-    end
-    timeperiod 
-  end
-   
-  tag "calendar:event:each:daterange" do |tag|
-    if tag.locals.event.start_date.strftime("%x") == tag.locals.event.end_date.strftime("%x") then
-      daterange = tag.locals.event.start_date.strftime(tag.attr['format'])
-    else
-      daterange = tag.locals.event.start_date.strftime(tag.attr['format']) << (tag.attr["separator"] || " - ") << tag.locals.event.end_date.strftime(tag.attr['format'])
-    end
-    daterange
-  end
-   
+
+
+  desc %{  <r:calendar:event:each:start format=""/>
+          The begin date of this event formatted according to the format attribute (there is no default value). }
   tag "calendar:event:each:start" do |tag|
     start_date = tag.locals.event.start_date.strftime(tag.attr['format'])
   end
    
+  desc %{  <r:calendar:event:each:end format="" />
+          The begin date of this event formatted according to the format attribute (there is no default value). }
   tag "calendar:event:each:end" do |tag|
     end_date = tag.locals.event.end_date.strftime(tag.attr['format'])
   end
+
+   desc %{  <r:calendar:event:each:timeperiod format="" separator="" />
+           The daterange of the event. Outputs "All Day" if 12-12 otherwise the begin_date - end_date range 
+           formatted according to the format string (no default). The separator between begin and end date 
+           defaults to - but can be set with the separator attribute. }
+   tag "calendar:event:each:daterange" do |tag|
+     if tag.locals.event.start_date.strftime("%x") == tag.locals.event.end_date.strftime("%x") then
+       daterange = tag.locals.event.start_date.strftime(tag.attr['format'])
+     else
+       daterange = tag.locals.event.start_date.strftime(tag.attr['format']) << (tag.attr["separator"] || " - ") << tag.locals.event.end_date.strftime(tag.attr['format'])
+     end
+     daterange
+   end
+
+
+   # desc %{  <r:calendar:event:each:timeperiod format="">...</r:calendar:event:each:timeperiod>
+   #         The timeperiod of the event. "All Day" or the formatted range e.g. begin_date - end_date }
+   # tag "calendar:event:each:timeperiod" do |tag|
+   #   if tag.locals.event.allday then
+   #     timeperiod = "All Day"
+   #   else
+   #     timeperiod = tag.locals.event.start_date.strftime(tag.attr['format']) << " - " << tag.locals.event.end_date.strftime(tag.attr['format'])
+   #   end
+   #   timeperiod 
+   # end
+   
 end
