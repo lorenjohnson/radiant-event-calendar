f53d4c7fe60124bc74b18ee223582410c4310576
deploy <deploy@126ae6b6-1cb0-44cf-83d6-85411bde8332>
2007-06-13 22:14:13 +0000

    Fixed handling of published? and live? in EventCalendarPage find_by_url
    git-svn-id: svn+ssh://svn.hellovenado.com/var/svn/tribe_cms_extensions/event_calendar@49 126ae6b6-1cb0-44cf-83d6-85411bde8332

--- app/models/event_calendar_page.rb
+++ app/models/event_calendar_page.rb
@@ -14,11 +14,11 @@ class EventCalendarPage < Page
 
   def find_by_url(url, live = true, clean = false)    
     url = clean_url(url) if clean
-    if url =~ %r{^#{ self.url }}
+    if (url =~ %r{^#{ self.url }}) && (not live or published?)
       self
     else
       super
     end
   end
-
+  
 end
